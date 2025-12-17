cd6dee3c6af2add8df8ab1fb61520b791cd8ea5c
Aryan Nandanwar <aryannandanwar738@gmail.com>
2025-12-17 19:46:00 +0530

    Setting up the project

---
+++ src/app/read.js
@@ -0,0 +1,15 @@
+/*
+This is the blog app
+
+i have changed the daark mode theme colors in globals.css file
+and added a color to inherit from parent to child in page.js file
+
+also in layout file i have wrapped children with a div container
+and wrapper classnames for styling purpose
+
+in globals.css file
+i have adjusted the styling of wrapper and layout-container classes
+
+
+
+*/
